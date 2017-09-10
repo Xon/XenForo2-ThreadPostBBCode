5c72d7205a244c1f7b84fb9f7d21260ef4715b82
Xon <tazipper1@hotmail.com>
2017-09-11 07:51:32 +0800

    Remove unused code

--- upload/src/addons/SV/ThreadPostBBCode/StaticGlobals.php
+++ upload/src/addons/SV/ThreadPostBBCode/StaticGlobals.php
@@ -4,9 +4,6 @@ namespace SV\ThreadPostBBCode;
 
 abstract class StaticGlobals
 {
-	public static $linkedThreadIds = [];
-	public static $linkedPostIds = [];
-
 	public static $threadIds = [];
 	public static $postIds = [];
 }
--- upload/src/addons/SV/ThreadPostBBCode/_output/extension_hint.php
+++ upload/src/addons/SV/ThreadPostBBCode/_output/extension_hint.php
@@ -3,11 +3,6 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
-namespace SV\ThreadPostBBCode\XF\BbCode\ProcessorAction
-{
-	class XFCP_AnalyzeUsage extends \XF\BbCode\ProcessorAction\AnalyzeUsage {}
-}
-
 namespace SV\ThreadPostBBCode\XF\BbCode
 {
 	class XFCP_RuleSet extends \XF\BbCode\RuleSet {}
